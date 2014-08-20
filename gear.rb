1bbcdf2919a79cc961028689f84f83c02c5c91da
raluka <badoi.raluca@gmail.com>
2014-08-20 16:15:24 +0200

    simple ruby script to calculate gear ratio

---
+++ gear.rb
@@ -0,0 +1,11 @@
+# simple ruby script to calculate gear ratio
+chainring = 52
+cog = 11
+ratio = chainring / cog.to_f
+puts ratio
+
+chainring = 30
+cog = 27
+ratio = chainring / cog.to_f
+puts ratio
+
